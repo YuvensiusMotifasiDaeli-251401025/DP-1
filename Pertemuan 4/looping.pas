3bcf0f0e2cca56862a83c6362b3c3d28818dfb27
YuvensiusMotifasiDaeli-251401025 <yuvensius1206@gmail.com>
2025-10-26 21:14:04 +0700

    Perulangan & Peloncatan

---
+++ Pertemuan 4/looping.pas
@@ -0,0 +1,84 @@
+program perulangan;
+uses crt;
+
+var 
+ i, j, n: integer;
+
+begin
+    clrscr;
+    // statement for
+
+    // for i:= 1  to 7 do
+    //     writeln('Fasilkom-TI');
+    // for i:= 1 to 10 do
+    //     writeln(i);
+    // for i:= 10 downto 1 do   
+    //     writeln(i);
+
+    // Nosted Loop
+
+    // write('Masukkan Jumlah Baris: ');
+    // readln(n);
+
+    // for i:= 1 to n do
+    // begin
+    //     for j:= 1 to i do
+    //     write(' *');
+    //     writeln;
+    // end;
+
+    // Latihan 1
+
+    // write('Masukkan jumlah baris: ');
+    // readln(n);
+
+    // for i:= 1 to n do
+    // begin 
+    //    for j:= 1 to i do
+    //    if i mod 2 = 0 then
+    //    begin 
+    //     write(i);
+    //     write(' ');
+    // end 
+    // else
+    // begin
+    //     write('* ' );
+    // end;
+    // writeln;
+    // end;
+
+    // segitiga Bintang
+    // write('Masukkan jumlah baris: ');
+    // readln(n);
+
+    // for i:= 1 to n do
+    // begin
+    //     for j:= 1 to n - i do
+    //     write('  ');
+
+    //     for j:= 1 to 2*i-1 do
+    //     write(' *');
+    //     writeln;
+    // end;
+
+    // statemnt while
+    // i := 1;
+    // while (i <= 5) do
+    // begin
+    //   writeln('IKLC !!');
+    //   inc(i, 2); 
+    // end;
+
+    // Statement Repeat - Until
+    // i := 0;
+    // repeat
+    //    i := i + 1;
+    //    writeln(i);
+    // until (i = 5);
+end.
+
+    
+
+
+
+    
